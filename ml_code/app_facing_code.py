7f1ac7ab05984bb2a58d63b94bec2f7f1c1f86e5
Asim Khan <Asimk21@users.noreply.github.com>
2018-05-10 13:36:20 +0100

    used getter to access env variable

--- ml_code/app_facing_code.py
+++ ml_code/app_facing_code.py
@@ -27,7 +27,7 @@ class AppFacing:
         self.driver.set_window_size(self.size['width'], self.size['height'])
 
        elif device_type=='mobile':
-        device_id = os.environ['ADB_DEVICE_ARGS']
+        device_id = os.environ.get('ADB_DEVICE_ARGS')
         
         if device_id is None:
             raise ValueError('Please set ADB_DEVICE_ARGS environment variable')
